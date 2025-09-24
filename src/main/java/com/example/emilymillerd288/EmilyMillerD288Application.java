f85281df3b66e3d66f8bbee9baeba065788c850a
Emily Miller <emily736@wgu.edu>
2025-09-24 15:52:49 -0500

    initial commit of backend programming project

---
+++ src/main/java/com/example/emilymillerd288/EmilyMillerD288Application.java
@@ -0,0 +1,13 @@
+package com.example.emilymillerd288;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmilyMillerD288Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmilyMillerD288Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emilymillerd288/EmilyMillerD288ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emilymillerd288;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmilyMillerD288ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
